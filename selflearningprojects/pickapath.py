b4aac4a2513dea6da9327abac6c6a28ae722237d
Andy H <andy.thanh.ha@gmail.com>
2022-08-31 13:26:22 +1200

    commit

--- selflearningprojects/pickapath.py
+++ selflearningprojects/pickapath.py
@@ -5,27 +5,27 @@ print ("************************************************\nWelcome to the game, E
 
 #StartingOptions
 print("1. Open the door to the thieves, telling them you will pretend you didn't see anything and hope they spare your life.")
-print("2. Go down the hall further away from the door.")
+print("2. Sprint down the hall further away from the door.")
 
 do= int(input(""))
 
 if do == 1:
-    print("The murders are not so merciful and decide to kill you. \n ***GAME OVER***")
+    print("The murderers are not so merciful and decide to kill you. \n ***GAME OVER***")
     exit()
 
 elif do == 2:
-    print("You make your way down the hall and the robbers are seconds away from bursting your front door open. You meet two doors at the end of the hall. Which door do you enter?")
-    print("1. Your bedroom door that has a window open to the fire escape.")
-    print("2. Your bathroom door that has the most sturdiest door in the apartment but no escape.")
+    print("You bolt down the hall and the thieves are seconds away from bursting your front door open. You meet two doors at the end of the hall. Which door do you enter?")
+    print("1. Your bedroom door.")
+    print("2. Your bathroom door.")
 
     do =int(input(""))
     if do == 1:
-        print("You enter your room but the robbers had just caught a glimpse of you whilst bursting open the door and proceed to run down the hall to chase you.")
+        print("You enter your room but the thieves had just caught a glimpse of you whilst bursting open the door and proceed to run down the hall.")
         print("Do you:\n1. Hide in your closet that looks too small to hide anything and hope they don't bother checking. \n2. Open your bedroom window and hop out to the fire escape.")
         
         do = int(input(""))
         if do == 1: 
-            print("You run into your master closet and hide in the closet amongst your dresses. The thieves enter into your bedroom and proceed to go into your closet and find you quickly. You are now dead. \n ***GAME OVER***")
+            print("You squeeze into your tiny closet amongst your dainty dresses and fat coats. The thieves enter into your bedroom and ravage around trying to find you. As they approach the closet your nervousness peaks and you break out in a scream. You are now dead. \n ***GAME OVER***")
         else:
             print("You hop onto the fire escape. Do you go UP or DOWN the escape?")
 
@@ -44,7 +44,7 @@ elif do == 2:
         if do == 1:
             print("As the thieves finally open the door after continuous barrage of kicks and body slams they are surprised to see you with a weapon. You shoot one of them but end up being tackled by the rest and die.\n***GAME OVER***")
         else: 
-            print("While sitting against the corner of the bathroom and reflecting on your life you notice that theres a weak layer against the wall. You begin to probe the wall and notice two bricks that can be fully indented and pushed into the wall. Which brick do you press into? LEFT or RIGHT?")  
+            print("While sitting against the corner of the bathroom and reflecting on your life you notice that some bricks are loose in the wall. You begin to probe the wall and notice two bricks that can be fully indented and pushed into the wall. Which brick do you press into? The LEFT BRICK or RIGHT BRICK?")  
             
             do = str(input(""))
             if do.lower() ==("left"):
@@ -52,3 +52,6 @@ elif do == 2:
                 print("The police have arrived and the thieves are captured. You survive.\n***YOU WIN***")
             else:
                 print("This brick turns on a hidden speaker system in the bathroom that plays jazz music. The thieves enter and dance on your body. \n***GAME OVER***")
+
+
+#Thank you for playing my game. Inspired by the show "Only Murders In The Building".
